1092dc78d184cdc31e4caacbf4f70239eb73f168
Joao Vargas <joao.francisco.vargas@gmail.com>
2023-10-04 15:22:10 +0200

    Added functions and test

---
+++ example.py
@@ -0,0 +1,11 @@
+
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a + b  # do not change this line until prompted to do so.
+
+
+def test_add():
+  assert add(3,4)     == 7
+  assert add("a","b") == "ab"
